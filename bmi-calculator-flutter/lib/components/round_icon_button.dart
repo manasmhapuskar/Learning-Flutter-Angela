199fc470d769b929b721a5aed39399339fec66b4
= <manasmhapuskar@gmail.com>
2020-08-12 19:42:37 -0500

    Added new icon to the app

---
+++ bmi-calculator-flutter/lib/components/round_icon_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class RoundIconButton extends StatelessWidget {
+
+  RoundIconButton({@required this.icon, @required this.onPress});
+
+  final IconData icon;
+  final Function onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Icon(icon ),
+      onPressed: onPress,
+      elevation: 6.0,
+      constraints: BoxConstraints.tightFor(
+        width: 45.0,
+        height: 45.0,
+      ),
+      shape: CircleBorder(),
+      fillColor: Color(0xFF4C4F5E),
+    );
+  }
+}
--- bmi-calculator-flutter/lib/main.dart
+++ bmi-calculator-flutter/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'input_page.dart';
+import 'screens/input_page.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -8,7 +8,7 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-          primaryColor: Color(0xFF0A0E21),
+          primaryColor: Color(0xFF0E1639),
           scaffoldBackgroundColor: Color(0xFF0A0E21),
       ),
       home: InputPage(),
